9b2afe41fdfa3b34275b6f89df3cd5a6bdb37879
Hisho Ohmasu <>
2020-07-24 23:38:36 +0900

    ✨ images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,20 @@
 import React from "react"
+import Picture from "../components/picture";
 
 const IndexPage = () => {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <div style={{
+        maxWidth: "1000px",
+        margin: '0 auto'
+      }}>
+        <Picture relativePath='screenshot.png' />
+        <Picture relativePath='test1/screenshot.png' />
+        <Picture relativePath='test2/deeptest2/screenshot.png' />
+        <Picture relativePath='test/no-iamge.png' />
+      </div>
+    </>
+  )
 }
 
 export default IndexPage
